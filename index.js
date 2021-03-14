fab84f78867a2cb084f99fecf3c35c60bf3793c1
Asante <asanterashid.biz@gmail.com>
2021-03-13 21:27:18 -0500

    re-ordered schema and started inquirer app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 // create the connection information for the sql database
 const connection = mysql.createConnection({
@@ -14,6 +15,27 @@ const connection = mysql.createConnection({
   password: 'root',
   database: 'company',
 });
+
+const start = () => {
+    inquirer
+      .prompt({
+        name: 'start',
+        type: 'list',
+        message: 'What would youi like to do?',
+        choices: ['Add Department', 'Add Role', 'Add Employee', 'View Departments', 'View Roles', 'View Employees', 'Update Employee Role'],
+      })
+      .then((answer) => {
+        // based on their answer, either call the bid or the post functions
+        if (answer.start === 'POST') {
+          postAuction();
+        } else if (answer.start === 'BID') {
+          bidAuction();
+        } else {
+          connection.end();
+        }
+      });
+  };
+
 
 connection.connect((err) => {
     if (err) throw err;
--- schema.sql
+++ schema.sql
@@ -3,22 +3,27 @@ DROP DATABASE IF EXISTS company;
 CREATE DATABASE company;
 USE company;
 
-CREATE TABLE employee (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary decimal(4,2),
-  PRIMARY KEY (id)
+  salary decimal(7,2),
+  PRIMARY KEY (id),
+  department_id int,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id),
+  role_id int,
+  manager_id int,
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
