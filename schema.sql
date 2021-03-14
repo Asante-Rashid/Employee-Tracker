1013645b91b0b238384d93a7850d93947dc975e2
Asante <asanterashid.biz@gmail.com>
2021-03-13 20:37:58 -0500

    added sql files and index server

---
+++ schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS company;
+
+CREATE DATABASE company;
+USE company;
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary decimal(4,2),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
